14b85435fb2c38096079a9dabaee0c9d3ad77c3c
Chong-JR <58478784+Chong-JR@users.noreply.github.com>
2020-03-31 17:41:46 +0200

    Update game.cpp

--- src/bulls_and_cows_lib/game.cpp
+++ src/bulls_and_cows_lib/game.cpp
@@ -14,16 +14,28 @@ namespace bulls_and_cows {
 
     void user_plays_against_computer(const GameOptions& game_options)
     {
-        std::cout << "TODO:\n"
-                     "    Create a board with a randomly generated secret code\n"
-                     "    DO\n"
-                     "       Display the board and the list of attempts so far\n"
-                     "       Ask the user to make another attempt\n"
-                     "       Compare the user's attempt with the secret code and deduce the number of bulls and cows\n"
-                     "       Add the user's attempt to the list of attempts of the board\n"
-                     "    WHILE not end of game\n"
-                     "    Display the board and the list of attempts so far\n"
-                     "    Display a message telling if the user won or lost\n";
+        Board board = create_board(game_options);
+        AttemptAndFeedback attempt_and_feedback;
+        do
+        {
+            display_board(std::cout, game_options, board);
+            attempt_and_feedback.attempt = ask_attempt(std::cout, std::cin, game_options, board);
+            attempt_and_feedback.feedback =
+                compare_attempt_with_secret_code(attempt_and_feedback.attempt, board.secret_code);
+            board.attempts_and_feedbacks.push_back(attempt_and_feedback);
+        } while (!is_end_of_game(game_options, board) && !is_win(game_options, board));
+        display_board(std::cout, game_options, board);
+
+        if (is_win(game_options, board))
+        {
+            std::cout << "Félicitations! Vous avez gagné !! Le code secret est: " << board.secret_code.value << std::endl;
+            
+        }
+        else
+        {
+            std::cout << "Dommage! Vous avez perdu...le code secret est: " << board.secret_code.value << std::endl;
+        }
+       
     }
 
     void computer_plays_against_computer(const GameOptions& game_options)
@@ -48,13 +60,80 @@ namespace bulls_and_cows {
 
     void configure_game_options(GameOptions& game_options)
     {
-        std::cout << "TODO:\n"
-                     "    DO\n"
-                     "       Display the current game options\n"
-                     "       Display the game options menu\n"
-                     "       Ask the user to type its choice\n"
-                     "       Treat the user's choice\n"
-                     "    UNTIL user's choice is to go back to main menu\n";
+        std::string path = "./save.txt";
+        std::ofstream save_file;
+        std::ifstream load_file(path);
+
+        GameOptionsMenuChoice options;
+        do
+        {
+            std::cout << "\n#################################\n";
+            display_game_options(std::cout, game_options);
+            std::cout << "\n#################################\n";
+            display_game_options_menu(std::cout);
+
+            GameOptionsMenuChoice choix = ask_game_options_menu_choice(std::cin);
+            options = choix;
+
+            switch (choix)
+            {
+            case GameOptionsMenuChoice::ModifyMaximumNumberOfAttempts:
+                std::cout << "\n Entrez le nombre maximum de tentatives: ";
+                game_options.max_number_of_attempts = ask_int_or_default(std::cin, game_options.max_number_of_attempts);
+                break;
+
+            case GameOptionsMenuChoice::ModifyNumberOfCharactersPerCode:
+                std::cout << "\n Entrez le nombre maximum de caractère par code: ";
+                game_options.number_of_characters_per_code = ask_int_or_default(std::cin, game_options.number_of_characters_per_code);
+                break;
+
+            case GameOptionsMenuChoice::ModifyMinimumAllowedCharacter:
+                std::cout << "\n Entrez la borne inférieure de l'éventail de caractères autorisés: ";
+                char min;
+                min = ask_char_or_default(std::cin, game_options.minimum_allowed_character);
+                if (min >= game_options.maximum_allowed_character)
+                {
+                    std::cout << "La borne inférieure ne peut pas être supérieure à la borne maximale";
+                }
+                else
+                game_options.minimum_allowed_character = min;
+                break;
+
+            case GameOptionsMenuChoice::ModifyMaximumAllowedCharacter:
+                std::cout << "\n Entrez la borne supérieure de l'éventail de caractères autorisés: ";
+                char max;
+                max = ask_char_or_default(std::cin, game_options.maximum_allowed_character);
+                if (max <= game_options.minimum_allowed_character)
+                {
+                    std::cout << "La borne supérieure ne peut pas être inférieure à la borne maximale";
+                }
+                else
+                    game_options.maximum_allowed_character = max;
+                break;
+
+            case GameOptionsMenuChoice::SaveOptions:
+                save_file.open(path);
+                if (!save_game_options(save_file, game_options))
+                {
+                    std::cout << "Erreur d'écriture dans le fichier.";
+                }
+                save_file.close();
+                break;
+
+            case GameOptionsMenuChoice::LoadOptions:
+                load_file.seekg(0, std::ios::beg);
+                if (!load_game_options(load_file, game_options))
+                {
+                    std::cout << "Erreur de lecture du fichier.";
+                }
+                break;
+
+            case GameOptionsMenuChoice::Error:
+                std::cout << "\n Vous n'avez pas entré un choix valide, veuillez réessayer. \n";
+                break;
+            }
+        } while (options != GameOptionsMenuChoice::BackToMain);
+        
     }
 
     void play_game()
@@ -89,3 +168,4 @@ namespace bulls_and_cows {
     }
 
 } // namespace bulls_and_cows
+
